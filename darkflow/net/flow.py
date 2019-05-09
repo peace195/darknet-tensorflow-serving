6290a121cd52855ce5b8a463d074e66725f9d596
Binh Do <binhtd.hust@gmail.com>
2019-05-09 20:58:32 +0700

    evaluate train

--- darkflow/net/flow.py
+++ darkflow/net/flow.py
@@ -21,13 +21,14 @@ train_stats = (
 )
 pool = ThreadPool()
 
+
 def _save_ckpt(self, step, loss_profile):
     file = '{}-{}{}'
     model = self.meta['name']
 
     profile = file.format(model, step, '.profile')
     profile = os.path.join(self.FLAGS.backup, profile)
-    with open(profile, 'wb') as profile_ckpt: 
+    with open(profile, 'wb') as profile_ckpt:
         pickle.dump(loss_profile, profile_ckpt)
 
     ckpt = file.format(model, step, '')
@@ -38,7 +39,8 @@ def _save_ckpt(self, step, loss_profile):
 
 def train(self):
     loss_ph = self.framework.placeholders
-    loss_mva = None; profile = list()
+    loss_mva = None;
+    profile = list()
 
     batches = self.framework.shuffle()
     loss_op = self.framework.loss
@@ -50,8 +52,8 @@ def train(self):
         ))
 
         feed_dict = {
-            loss_ph[key]: datum[key] 
-                for key in loss_ph }
+            loss_ph[key]: datum[key]
+            for key in loss_ph}
         feed_dict[self.inp] = x_batch
         feed_dict.update(self.feed)
 
@@ -74,25 +76,38 @@ def train(self):
         self.say(form.format(step_now, loss, loss_mva))
         profile += [(loss, loss_mva)]
 
-        ckpt = (i+1) % (self.FLAGS.save // self.FLAGS.batch)
+        ckpt = (i + 1) % (self.FLAGS.save // self.FLAGS.batch)
         args = [step_now, profile]
 
         if not ckpt:
-            predict(self)
-            mAP = evaluate(self, self.FLAGS.imgdir, os.path.join(self.FLAGS.imgdir, 'out'), "results_" + str(step_now) + ".txt")
+            # Evaluate trainset
+            predict(self, self.FLAGS.dataset)
+            evaluate(self, self.FLAGS.annotation, os.path.join(self.FLAGS.imgdir, 'out'),
+                     "results_train" + str(step_now) + ".txt")
+            '''
+            Maybe we have to delete 'out' folder for next evaluation of the testset. Please check!
+            For further convinience, we should change the postprocess function on predict.py 
+            because the default output is: os.path.join(self.FLAGS.imgdir, 'out')
+            Contact me on telegram for the details -_-
+            '''
+            # Evaluate testset
+            predict(self, self.FLAGS.imgdir)
+            mAP = evaluate(self, self.FLAGS.imgdir, os.path.join(self.FLAGS.imgdir, 'out'),
+                           "results_test" + str(step_now) + ".txt")
             if mAP > best_mAP:
                 best_mAP = mAP
                 _save_ckpt(self, *args)
 
     if ckpt: _save_ckpt(self, *args)
 
+
 def return_predict(self, im):
     assert isinstance(im, np.ndarray), \
-				'Image is not a np.ndarray'
+        'Image is not a np.ndarray'
     h, w, _ = im.shape
     im = self.framework.resize_input(im)
     this_inp = np.expand_dims(im, 0)
-    feed_dict = {self.inp : this_inp}
+    feed_dict = {self.inp: this_inp}
 
     out = self.sess.run(self.out, feed_dict)[0]
     boxes = self.framework.findboxes(out)
@@ -115,8 +130,8 @@ def return_predict(self, im):
     return boxesInfo
 
 
-def predict(self):
-    inp_path = self.FLAGS.imgdir
+def predict(self, path):
+    inp_path = path
     all_inps = os.listdir(inp_path)
     all_inps = [i for i in all_inps if self.framework.is_inp(i)]
     if not all_inps:
@@ -138,11 +153,12 @@ def predict(self):
                 os.path.join(inp_path, inp)), 0)), this_batch)
 
         # Feed to the net
-        feed_dict = {self.inp : np.concatenate(inp_feed, 0)}    
+        feed_dict = {self.inp: np.concatenate(inp_feed, 0)}
         self.say('Forwarding {} inputs ...'.format(len(inp_feed)))
         start = time.time()
         out = self.sess.run(self.out, feed_dict)
-        stop = time.time(); last = stop - start
+        stop = time.time();
+        last = stop - start
         self.say('Total time = {}s / {} inps = {} ips'.format(
             last, len(inp_feed), len(inp_feed) / last))
 
@@ -150,10 +166,11 @@ def predict(self):
         self.say('Post processing {} inputs ...'.format(len(inp_feed)))
         start = time.time()
         pool.map(lambda p: (lambda i, prediction:
-            self.framework.postprocess(
-               prediction, os.path.join(inp_path, this_batch[i])))(*p),
-            enumerate(out))
-        stop = time.time(); last = stop - start
+                            self.framework.postprocess(
+                                prediction, os.path.join(inp_path, this_batch[i])))(*p),
+                 enumerate(out))
+        stop = time.time();
+        last = stop - start
 
         # Timing
         self.say('Total time = {}s / {} inps = {} ips'.format(
@@ -161,7 +178,7 @@ def predict(self):
 
 
 def evaluate(self, test_dir, ret_dir, results_file_name):
-    MINOVERLAP = 0.5 # default value (defined in the PASCAL VOC2012 challenge)
+    MINOVERLAP = 0.5  # default value (defined in the PASCAL VOC2012 challenge)
     GT_PATH = os.path.join(os.getcwd(), test_dir)
     DR_PATH = os.path.join(os.getcwd(), ret_dir)
 
@@ -181,7 +198,7 @@ def evaluate(self, test_dir, ret_dir, results_file_name):
                 right = bndbox.find('xmax').text
                 bottom = bndbox.find('ymax').text
                 new_f.write("%s %s %s %s %s\n" % (obj_name, left, top, right, bottom))
-    
+
     json_list = glob.glob(DR_PATH + '/*.json')
     if len(json_list) == 0:
         print("Error: no .json files found in detection-results")
@@ -200,7 +217,6 @@ def evaluate(self, test_dir, ret_dir, results_file_name):
                     obj_name + " " + str(conf) + " " + str(left) + " " + str(top) + " " + str(right) + " " + str(
                         bottom) + '\n')
 
-
     def log_average_miss_rate(precision, fp_cumsum, num_images):
         if precision.size == 0:
             lamr = 0
@@ -215,7 +231,7 @@ def evaluate(self, test_dir, ret_dir, results_file_name):
         mr_tmp = np.insert(mr, 0, 1.0)
 
         # Use 9 evenly spaced reference points in log-space
-        ref = np.logspace(-2.0, 0.0, num = 9)
+        ref = np.logspace(-2.0, 0.0, num=9)
         for i, ref_i in enumerate(ref):
             j = np.where(fppi_tmp <= ref_i)[-1][-1]
             ref[i] = mr_tmp[j]
@@ -229,34 +245,33 @@ def evaluate(self, test_dir, ret_dir, results_file_name):
         print(msg)
         sys.exit(0)
 
-
     """
      Calculate the AP given the recall and precision array
         1st) We compute a version of the measured precision/recall curve with
              precision monotonically decreasing
         2nd) We compute the AP as the area under this curve by numerical integration.
     """
+
     def voc_ap(rec, prec):
-        rec.insert(0, 0.0) # insert 0.0 at begining of list
-        rec.append(1.0) # insert 1.0 at end of list
+        rec.insert(0, 0.0)  # insert 0.0 at begining of list
+        rec.append(1.0)  # insert 1.0 at end of list
         mrec = rec[:]
-        prec.insert(0, 0.0) # insert 0.0 at begining of list
-        prec.append(0.0) # insert 0.0 at end of list
+        prec.insert(0, 0.0)  # insert 0.0 at begining of list
+        prec.append(0.0)  # insert 0.0 at end of list
         mpre = prec[:]
 
-        for i in range(len(mpre)-2, -1, -1):
-            mpre[i] = max(mpre[i], mpre[i+1])
+        for i in range(len(mpre) - 2, -1, -1):
+            mpre[i] = max(mpre[i], mpre[i + 1])
 
         i_list = []
         for i in range(1, len(mrec)):
-            if mrec[i] != mrec[i-1]:
-                i_list.append(i) # if it was matlab would be i + 1
+            if mrec[i] != mrec[i - 1]:
+                i_list.append(i)  # if it was matlab would be i + 1
 
         ap = 0.0
         for i in i_list:
-            ap += ((mrec[i]-mrec[i-1])*mpre[i])
+            ap += ((mrec[i] - mrec[i - 1]) * mpre[i])
         return ap, mrec, mpre
-
 
     def file_lines_to_list(path):
         # open txt file lines to a list
@@ -299,10 +314,10 @@ def evaluate(self, test_dir, ret_dir, results_file_name):
         for line in lines_list:
             try:
                 if "difficult" in line:
-                        class_name, left, top, right, bottom, _difficult = line.split()
-                        is_difficult = True
+                    class_name, left, top, right, bottom, _difficult = line.split()
+                    is_difficult = True
                 else:
-                        class_name, left, top, right, bottom = line.split()
+                    class_name, left, top, right, bottom = line.split()
             except ValueError:
                 error_msg = "Error: File " + txt_file + " in the wrong format.\n"
                 error_msg += " Expected: <class_name> <left> <top> <right> <bottom> ['difficult']\n"
@@ -312,27 +327,26 @@ def evaluate(self, test_dir, ret_dir, results_file_name):
                 error(error_msg)
             # check if class is in the ignore list, if yes skip
 
-            bbox = left + " " + top + " " + right + " " +bottom
+            bbox = left + " " + top + " " + right + " " + bottom
             if is_difficult:
-                    bounding_boxes.append({"class_name":class_name, "bbox":bbox, "used":False, "difficult":True})
-                    is_difficult = False
+                bounding_boxes.append({"class_name": class_name, "bbox": bbox, "used": False, "difficult": True})
+                is_difficult = False
             else:
-                    bounding_boxes.append({"class_name":class_name, "bbox":bbox, "used":False})
-                    # count that object
-                    if class_name in gt_counter_per_class:
-                        gt_counter_per_class[class_name] += 1
+                bounding_boxes.append({"class_name": class_name, "bbox": bbox, "used": False})
+                # count that object
+                if class_name in gt_counter_per_class:
+                    gt_counter_per_class[class_name] += 1
+                else:
+                    # if class didn't exist yet
+                    gt_counter_per_class[class_name] = 1
+
+                if class_name not in already_seen_classes:
+                    if class_name in counter_images_per_class:
+                        counter_images_per_class[class_name] += 1
                     else:
                         # if class didn't exist yet
-                        gt_counter_per_class[class_name] = 1
-
-                    if class_name not in already_seen_classes:
-                        if class_name in counter_images_per_class:
-                            counter_images_per_class[class_name] += 1
-                        else:
-                            # if class didn't exist yet
-                            counter_images_per_class[class_name] = 1
-                        already_seen_classes.append(class_name)
-
+                        counter_images_per_class[class_name] = 1
+                    already_seen_classes.append(class_name)
 
         # dump bounding_boxes into a ".json" file
         with open(TEMP_FILES_PATH + "/" + file_id + "_ground_truth.json", 'w') as outfile:
@@ -349,9 +363,9 @@ def evaluate(self, test_dir, ret_dir, results_file_name):
     for class_index, class_name in enumerate(gt_classes):
         bounding_boxes = []
         for txt_file in dr_files_list:
-            #print(txt_file)
+            # print(txt_file)
             # the first time it checks if all the corresponding ground-truth files exist
-            file_id = txt_file.split(".txt",1)[0]
+            file_id = txt_file.split(".txt", 1)[0]
             file_id = os.path.basename(os.path.normpath(file_id))
             temp_path = os.path.join(GT_PATH, (file_id + ".txt"))
             if class_index == 0:
@@ -369,10 +383,10 @@ def evaluate(self, test_dir, ret_dir, results_file_name):
                     error_msg += " Received: " + line
                     error(error_msg)
                 if tmp_class_name == class_name:
-                    bbox = left + " " + top + " " + right + " " +bottom
-                    bounding_boxes.append({"confidence":confidence, "file_id":file_id, "bbox":bbox})
+                    bbox = left + " " + top + " " + right + " " + bottom
+                    bounding_boxes.append({"confidence": confidence, "file_id": file_id, "bbox": bbox})
         # sort detection-results by decreasing confidence
-        bounding_boxes.sort(key=lambda x:float(x['confidence']), reverse=True)
+        bounding_boxes.sort(key=lambda x: float(x['confidence']), reverse=True)
         with open(TEMP_FILES_PATH + "/" + class_name + "_dr.json", 'w') as outfile:
             json.dump(bounding_boxes, outfile)
 
@@ -391,7 +405,7 @@ def evaluate(self, test_dir, ret_dir, results_file_name):
         dr_file = TEMP_FILES_PATH + "/" + class_name + "_dr.json"
         dr_data = json.load(open(dr_file))
         nd = len(dr_data)
-        tp = [0] * nd # creates an array of zeros of size nd
+        tp = [0] * nd  # creates an array of zeros of size nd
         fp = [0] * nd
         for idx, detection in enumerate(dr_data):
             file_id = detection["file_id"]
@@ -402,18 +416,18 @@ def evaluate(self, test_dir, ret_dir, results_file_name):
             ovmax = -1
             gt_match = -1
             # load detected object bounding-box
-            bb = [ float(x) for x in detection["bbox"].split() ]
+            bb = [float(x) for x in detection["bbox"].split()]
             for obj in ground_truth_data:
                 # look for a class_name match
                 if obj["class_name"] == class_name:
-                    bbgt = [ float(x) for x in obj["bbox"].split() ]
-                    bi = [max(bb[0],bbgt[0]), max(bb[1],bbgt[1]), min(bb[2],bbgt[2]), min(bb[3],bbgt[3])]
+                    bbgt = [float(x) for x in obj["bbox"].split()]
+                    bi = [max(bb[0], bbgt[0]), max(bb[1], bbgt[1]), min(bb[2], bbgt[2]), min(bb[3], bbgt[3])]
                     iw = bi[2] - bi[0] + 1
                     ih = bi[3] - bi[1] + 1
                     if iw > 0 and ih > 0:
                         # compute overlap (IoU) = area of intersection / area of union
                         ua = (bb[2] - bb[0] + 1) * (bb[3] - bb[1] + 1) + (bbgt[2] - bbgt[0]
-                                        + 1) * (bbgt[3] - bbgt[1] + 1) - iw * ih
+                                                                          + 1) * (bbgt[3] - bbgt[1] + 1) - iw * ih
                         ov = iw * ih / ua
                         if ov > ovmax:
                             ovmax = ov
@@ -431,7 +445,7 @@ def evaluate(self, test_dir, ret_dir, results_file_name):
                         count_true_positives[class_name] += 1
                         # update the ".json" file
                         with open(gt_file, 'w') as f:
-                                f.write(json.dumps(ground_truth_data))
+                            f.write(json.dumps(ground_truth_data))
                     else:
                         fp[idx] = 1
             else:
@@ -455,10 +469,10 @@ def evaluate(self, test_dir, ret_dir, results_file_name):
 
         ap, mrec, mprec = voc_ap(rec[:], prec[:])
         sum_AP += ap
-        text = "{0:.2f}%".format(ap*100) + " = " + class_name + " AP " #class_name + " AP = {0:.2f}%".format(ap*100)
+        text = "{0:.2f}%".format(ap * 100) + " = " + class_name + " AP "  # class_name + " AP = {0:.2f}%".format(ap*100)
 
-        rounded_prec = [ '%.2f' % elem for elem in prec ]
-        rounded_rec = [ '%.2f' % elem for elem in rec ]
+        rounded_prec = ['%.2f' % elem for elem in prec]
+        rounded_rec = ['%.2f' % elem for elem in rec]
         results_file.write(text + "\n Precision: " + str(rounded_prec) + "\n Recall :" + str(rounded_rec) + "\n\n")
         print(text)
         ap_dictionary[class_name] = ap
@@ -467,10 +481,9 @@ def evaluate(self, test_dir, ret_dir, results_file_name):
         lamr, mr, fppi = log_average_miss_rate(np.array(rec), np.array(fp), n_images)
         lamr_dictionary[class_name] = lamr
 
-
     results_file.write("\n# mAP of all classes\n")
     mAP = sum_AP / n_classes
-    text = "mAP = {0:.2f}%".format(mAP*100)
+    text = "mAP = {0:.2f}%".format(mAP * 100)
     results_file.write(text + "\n")
     print(text)
 
